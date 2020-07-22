681e6a9e0e5d13e8179fbab0d472bcc135c180bd
Paulo Lima <paulobruny@gmail.com>
2020-07-22 17:51:16 -0300

    add pwa configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ function App({ Component, pageProps }: AppProps) {
           name="description"
           content="A single project started to work with TS, React and Next"
         />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
